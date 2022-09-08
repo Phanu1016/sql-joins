e1b9b08ad8faa65e1203936915a00cafcc3e238a
Phanu <admin@phanuphanu.com>
2022-09-08 04:00:09 -0700

    finished

--- queries.sql
+++ queries.sql
@@ -1,1 +1,10 @@
 -- write your queries here
+
+sqlzoo_6 copy-- Join the two tables so that every column and record appears, regardless of if there is not an owner_id.
+SELECT * FROM owners LEFT JOIN vehicles ON owners.id = vehicles.owner_id;
+
+-- Count the number of cars for each owner. Display the owners first_name, last_name and count of vehicles. The first_name should be ordered in ascending order.
+SELECT first_name, last_name, COUNT(owners.id) FROM owners JOIN vehicles ON owners.id = vehicles.owner_id GROUP BY owners.id ORDER BY first_name;
+
+-- Count the number of cars for each owner and display the average price for each of the cars as integers. Display the owners first_name, last_name, average price and count of vehicles. The first_name should be ordered in descending order. Only display results with more than one vehicle and an average price greater than 10000.
+SELECT first_name, last_name, ROUND(AVG(vehicles.price)) AS average_price, COUNT(owners.id) FROM owners JOIN vehicles ON owners.id = vehicles.owner_id GROUP BY owners.id HAVING COUNT(owners.id) > 1 AND AVG(vehicles.price) > 10000 ORDER BY first_name DESC;
